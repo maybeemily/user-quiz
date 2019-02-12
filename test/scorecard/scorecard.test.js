32b0dc8ac03a7eb6f3313d783b5eb2eb83f5c80d
emily baier <emilyrbaier@gmail.com>
2019-02-12 13:29:48 -0800

    changed messages so tests pass

--- test/scorecard/scorecard.test.js
+++ test/scorecard/scorecard.test.js
@@ -8,20 +8,20 @@ QUnit.module('scorecard');
 
 test('if polarVortex has most points, return polarVortex', function(assert){
     const result = weatherResult({ polarVortex: 1, heatwave: 0, rainbow: 0, cloudyMeatballs: 0 });
-    assert.equal(result, 'polarVortex');
+    assert.equal(result, 'You are a Polar Vortex!');
 });
 
 test('if heatwave has most points, return heatwave', function(assert){
     const result = weatherResult({ polarVortex: 0, heatwave: 1, rainbow: 0, cloudyMeatballs: 0 });
-    assert.equal(result, 'heatwave');
+    assert.equal(result, 'You are a Heatwave!');
 });
 
 test('if rainbow has most points, return rainbow', function(assert){
     const result = weatherResult({ polarVortex: 0, heatwave: 0, rainbow: 1, cloudyMeatballs: 0 });
-    assert.equal(result, 'rainbow');
+    assert.equal(result, 'You are a Double Rainbow!');
 });
 
 test('if cloudyMeatballs has most points, return cloudyMeatballs', function(assert){
     const result = weatherResult({ polarVortex: 0, heatwave: 0, rainbow: 0, cloudyMeatballs: 1 });
-    assert.equal(result, 'cloudyMeatballs');
+    assert.equal(result, 'You are Cloudy with a Chance of Meatballs!');
 });
